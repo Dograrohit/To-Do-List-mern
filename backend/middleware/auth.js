58a891e4222be526c172c5f9071e331835201c15
sastanoobie <sastanoobie2@gmail.com>
2025-08-29 22:06:58 +0530

    auth fix

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,14 +1,13 @@
 const jwt = require("jsonwebtoken")
 
 function auth(req,res,next){
-      const token = req.cookies.token;
+   const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; 
 
-      if(!token){
-         return res.status(401).json({
-            message:'Unauthorized'
-        })
-      }
-
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+     
       try{
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
         req.user =  decoded
